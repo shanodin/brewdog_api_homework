1a1e522b7c428a0ad08514a4c14ea051c9b03460
Alice Rees <alice.rees90@gmail.com>
2017-10-18 20:29:47 +0100

    extract map beer names function

--- brewdog_app.js
+++ brewdog_app.js
@@ -1,12 +1,20 @@
-var getDataFromBrewdog = function() {
+var getBeerDataFromBrewdog = function() {
   var url = 'https://api.punkapi.com/v2/beers';
   var request = new XMLHttpRequest();
   request.open("GET", url);
-  request.addEventListener("load", function() {
-    var brewdogData = JSON.parse(this.responseText);
-    console.log(brewdogData);
-  })
+  request.addEventListener("load", mapBeerNames)
   request.send();
 }
 
-getDataFromBrewdog();
+
+var mapBeerNames = function() {
+  var apiResult = this;
+  var beers = JSON.parse(apiResult.responseText);
+  var beerNames = beers.map(function(beer){
+    return beer.name
+  })
+  debugger
+  return beerNames;
+}
+
+getBeerDataFromBrewdog();
